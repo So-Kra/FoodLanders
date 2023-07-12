c2057271c300f03411211d69d3e7c265c17dad31
Sotokan <sotokan@Xubuntu.myguest.virtualbox.org>
2023-07-12 20:12:58 +0300

    trial

--- indexNavigation.js
+++ indexNavigation.js
@@ -36,3 +36,31 @@ function redirectToLink(link) {
 // Event listener for input change
 document.getElementById('searchInput').addEventListener('input', search);
 
+
+function includeHTML(file, elementId) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById(elementId).innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", file, true);
+  xhttp.send();
+}
+
+includeHTML('header.html', 'header');
+includeHTML('footer.html', 'footer');
+includeHTML('/home/sotokan/repos/foodlanders_bkp2/header.html', 'headerDeepPath');
+includeHTML('/home/sotokan/repos/foodlanders_bkp2/footer.html', 'footerDeepPath');
+
+
+
+$(function () {
+  var includes = $('[data-include]')
+  $.each(includes, function () {
+    var file = '../' + $(this).data('include') + '.html'
+    $(this).load(file)
+  })  
+})
+
+
